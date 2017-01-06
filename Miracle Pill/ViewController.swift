a804e6dffea60cb465988c5243bd3ba9f6265677
MinuraIddamalgoda <mini2476@gmail.com>
2017-01-06 23:47:48 +1100

    first commit

---
+++ Miracle Pill/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Miracle Pill
+//
+//  Created by Minura Iddamalgoda on 5/1/17.
+//  Copyright © 2017 SchemeSeventeen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - Outlets
+    
+    @IBOutlet weak var statePickButton: UIButton!
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - Actions
+    @IBAction func statePickButton(_ sender: UIButton) {
+        
+    }
+
+}
+
