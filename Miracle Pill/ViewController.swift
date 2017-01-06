039709cb97e4a05057355dcb23a7729dc4bff59e
MinuraIddamalgoda <mini2476@gmail.com>
2017-01-07 00:21:40 +1100

    Updated UI Views and added a temporary list of states to choose from

--- Miracle Pill/ViewController.swift
+++ Miracle Pill/ViewController.swift
@@ -8,22 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController:
+    UIViewController,
+    UIPickerViewDataSource,
+    UIPickerViewDelegate {
 
-    // MARK: - Outlets
+    // Instance vars
+    let states: [String] = ["Alaska", "Arkansas", "Alabama", "California", "Maine", "New York"]
     
-    @IBOutlet weak var statePickButton: UIButton!
+    // MARK: - Outlets
     @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
     
     // MARK: - Actions
-    @IBAction func statePickButton(_ sender: UIButton) {
+    @IBAction func stateButtonPressed(_ sender: UIButton) {
+        statePicker.isHidden = false
         
     }
-
+    
+    // MARK: - Delegate Methods
+    // MARK: PickerView
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerButton.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
 }
 
